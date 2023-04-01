11092009dc1a2188b53514fa8635ac8952f5f9c6
Uriel Parra <urielparra@gmail.com>
2023-04-01 12:32:39 -0500

    Fixing error in scr/utils.py

--- src/utils.py
+++ src/utils.py
@@ -26,5 +26,6 @@ def get_model_performance_test_set(y_real: pd.Series, y_pred: pd.Series) ->None:
     sns.regplot(x=y_pred, y=y_real, ax = ax)
     ax.set_xlabel('Predicted worldwide gross')
     ax.set_ylabel('Real worldwide gross')
-    ax.set_title('Behavior of model predic
+    ax.set_title('Behavior of model prediction')
+    fig.savefig('prediction_behavior.png')
     

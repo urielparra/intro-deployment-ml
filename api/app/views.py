4c60be00ee4611ca49666151fa7a9835d211a294
Uriel Parra <urielparra@gmail.com>
2023-03-31 20:55:08 -0500

    adding testing

--- api/app/views.py
+++ api/app/views.py
@@ -1,7 +1,7 @@
 from .models import PredictionRequest
 from .utils import get_model, transform_to_dataframe
 
-model = None
+model = get_model()
 
 def get_prediction(request: PredictionRequest) -> float:
     data_to_predict = transform_to_dataframe(request)
--- api/main.py
+++ api/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from .app.models import PredictionResponse, PredictionRequest
 from .app.views import get_prediction
 
-app = FastAPI(doc_url='/')
+app = FastAPI(docs_url='/')
 
 @app.post('/v1/prediction')
 def  made_model_prediction(request: PredictionRequest):
-    return PredictionResponse(worldwide_Gross=get_prediction(request))
+    return PredictionResponse(worldwide_gross=get_prediction(request))
